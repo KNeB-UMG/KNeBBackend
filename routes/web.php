e7233a7bcae5f9fca59a6b2f869085fbc33be328
Kacper Kmiecik <kacper.kmiecik2003@gmail.com>
2024-12-07 21:17:15 +0100

    preparing member endpoints:

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('api/technical/basic', [TechnicalController::class, 'getBasic']);
+// routes for managing user
+Route::prefix('api/member')->name('api/member.')->group(function () {
+    Route::post('/register', [TechnicalController::class, 'registerMember']);
+    Route::post('/login', [TechnicalController::class, 'loginMember']);
+    Route::post('/deactivate', [TechnicalController::class, 'deactivateMember']);
+    Route::post('/edit', [TechnicalController::class, 'editMember']);
+    Route::post('/delete', [TechnicalController::class, 'deleteMember']);
+    Route::post('/activate', [TechnicalController::class, 'activateMember']);
+    Route::post('/changerole', [TechnicalController::class, 'changeRoleOfMember']);
+});
+
+
+
